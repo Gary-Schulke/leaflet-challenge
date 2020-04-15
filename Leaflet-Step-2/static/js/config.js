83ddff9a017c9b1b8c29f1fa124fc3a6442daed6
gary-schulke <gary.schulke@outlook.com>
2020-04-15 16:54:33 -0700

    fixed key

--- Leaflet-Step-2/static/js/config.js
+++ Leaflet-Step-2/static/js/config.js
@@ -1,2 +1,5 @@
 // API key
-const API_KEY = "";
+// alert("Add your API key in config.js and remove this alert")
+const API_KEY = "pk.eyJ1IjoiZ2FyeXNjaHVsa2UiLCJhIjoiY2s0enc4bnpyMDg5NTNtbzB5d3RkNGExOSJ9.RL2z_jZO5LmlNuw-s4A7jw";
+const MAPBOX_URL = "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}";
+/*const ATTRIBUTION = "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>";*/
